e8f6af0340ff87ef43e7e4ad5fe5ec1beb353440
vineeth <you@example.com>
2022-09-06 17:59:41 +0530

    test3

--- output.tf
+++
@@ -1,9 +0,0 @@
-
-
-output "zones"{
-   value = data.aws_availability_zones.available.names
-}
-
-output "countofaz"{
-    value = length(data.aws_availability_zones.available.names)
-}
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "pub_cidr"{
-    default = ["10.1.0.0/24","10.1.1.0/24","10.1.2.0/24"]
-    type = list
-}
-
-variable "private_cidr"{
-    default = ["10.1.3.0/24","10.1.4.0/24","10.1.5.0/24"]
-    type = list
-}
